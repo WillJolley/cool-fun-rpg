5dd35b274d046a101c0d527caf2ef4fe7dbe6d01
WillJolley <yeswilljolley@gmail.com>
2023-07-26 18:56:30 -0700

    add battle functionality

--- src/rpg.js
+++ src/rpg.js
@@ -16,6 +16,11 @@ export class Character {
   level = 1;
   dice;
 
+  generateCharacter(type, items) {
+    const newCharacter = new Character(type, items);
+    this.characters.push(newCharacter);
+  }
+
   setAttackPower() {
     this.dice = Math.floor(Math.random() * 10) + 1;
     this.attackPower += this.dice;
@@ -30,6 +35,12 @@ export class Character {
     this.dice = Math.floor(Math.random() * 10) + 1;
     this.armor += this.dice;
   } 
+
+  calcHealthLoss(damage) {
+    const netDamage = damage - this.armor / 2;
+    this.hp -= netDamage;
+
+  }
 }
 
 export class Enemy {
@@ -37,36 +48,55 @@ export class Enemy {
     this.name = name
   }
 
+  isAlive = true;
   hp = Math.floor(Math.random() * 200) + 1;
   attackPower = Math.floor(Math.random() * 20) + 1;
   armor = Math.floor(Math.random() * 20) + 1;
   speed = Math.floor(Math.random() * 20) + 1;
-  
+
+  calcHealthLoss(damage) {
+    const netDamage = damage - this.armor / 2;
+    this.hp -= netDamage;
+
+  }
+
 }
 
 export class Game {
-  constructor() {
-    this.players = [new Character(0)];
-    this.activePlayer = 0;
-    this.dice = 0;
+  /**
+   * @param {Character[]} players
+   */
+  constructor(players) {
+    this.players = players;
   }
 
   
+  /**
+   * @type {Enemy[]}
+  */
+ enemies = []
+ 
+  /**
+    * @type {Character}
+  */
+  activePlayer;
+  dice = 0;
 
-  // const enemyName = {""}
+  nextTurn() {
+    const player = this.players[this.activePlayer];
+    const enemy = this.enemies[0];
+
+    enemy.calcHealthLoss(player.attackPower);
+
+    if(enemy <= 0) {
+      this.enemies.shift();
+    }
+
+    this.activePlayer = (this.activePlayer + 1) % this.players.length;
+  }
+  generateEnemy(name) {
+    const newEnemy = new Enemy(name);
+    this.enemies.push(newEnemy);
   }
 
-
-//const humanCharacter = new Character("Human", ["armor"])
-//humanCharacter.attackPower = 20;
-//humanCharacter.hp = 100;
-//humanCharacter.level = 1;
-
-//const alienCharacter = new Character("Alien",["armor"])
-
-const game = new Game ();
-
-//generateEnemy() {
-//const enemy = game.generateEnemy();
-//const
-
+}
